76197e97749a692b5d578244bf139ed50dcc4de5
mazzobeg <mazzobel.gvn@gmail.com>
2024-02-10 12:51:12 +0100

    create a dedicated space for aria-backend

--- frontend/src/components/ArticlesRoot.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>Articles</h1>
-    <ArticlesList />
-</template>
-
-<script lang="ts" setup>
-import ArticlesList from './articles/ArticlesList.vue';
-
-</script>
--- frontend/src/components/ScrapersRoot.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>Scrapers</h1>
-    <ScrapersList/>
-</template>
-
-<script lang="ts" setup>
-import ScrapersList from '@/components/scrapers/ScrapersList.vue'
-</script>
