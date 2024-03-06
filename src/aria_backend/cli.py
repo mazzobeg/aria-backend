60c8022dc809738c27e368742fd2b161bc0c2ebb
mazzobeg <mazzobel.gvn@gmail.com>
2024-03-06 13:49:48 +0100

    Refactor CLI module to improve configuration handling

--- src/aria_backend/cli.py
+++ src/aria_backend/cli.py
@@ -1,4 +1,4 @@
-from . import create_app, init_db
+from . import create_app
 import os, sys
 import logging as log
 import urllib.parse
@@ -15,22 +15,15 @@ def main():
     args = parser.parse_args()
     config_path = args.config
 
+    # check if the file is .json
+    if not config_path.endswith(".json"):
+        log.error("Configuration file must be a .json file")
+        sys.exit(1)
+
     with open(config_path, "r") as f:
-        config = json.load(f)
-
-        # executable_path = os.path.dirname(sys.executable)
-        # log.info(f"Executable path: {executable_path}")
-
-        app = create_app(config=config)
-
-        # assume the path is absolute
-        db_path = urllib.parse.urlparse(app.config["SQLALCHEMY_DATABASE_URI"]).path
-        log.info(f"Database path: {db_path}")
-        if not os.path.exists(db_path):
-            log.info("Database not found, creating a new one")
-            with app.app_context():
-                init_db()
-        app.run()
+        config_as_dict = json.load(f)
+        app = create_app(config_as_dict)
+        app.run(port=app.config["FLASK_RUN_PORT"], host="0.0.0.0")
 
 
 if __name__ == "__main__":
